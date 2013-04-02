3088c0e968e186704c6fc203886613efaa60f436
Shaun Ruffell <sruffell@digium.com>
2013-04-02 13:47:58 -0500

    menu-example: Update to the 1.3 firmware interface.
    From [1]:
    
      * A digium.event namespace has been created that replaces the
        previous implementation of registering event callbacks through the
        properties of digium.handlers. Methods were added to digium.event to
        observe, trigger, and stop observing events. This allows multiple
        handlers to subscribe to the same event.
    
      * Apps control whether they are shutdown when they are hidden with
        digium.app.exitAfterBackground.
    
    [1] http://phones.digium.com/content/phone-api-production-firmware-13-changes-beta-12

--- menu-example/startup.js
+++ menu-example/startup.js
@@ -14,74 +14,77 @@ function getInfo(selection) {
     print("Getting info for " + selection + "\n");
 }
 
-digium.handlers.onstart = function() {
-    var handler = {};
-    //This handler is called when any assigned softkeys/hardkeys are pressed
-    //It receives the id of the selected item and the action as parameters
-    handler.processMenuAction = function (params) {
-        switch (params.actionId) {
-        case 'select':
-            selectCallback(params.selectionId);
-            break;
-        case 'help':
-            helpCallback(params.selectionId);
-            break;
-        case 'getInfo':
-            getInfo(params.selectionId);
-            break;
-        case 'exit':
-        default:
-            digium.background();
-            break;
-        }
-    };
-    //the menu items are defined in an array
-    var items = [
-        {
-            text  : 'Menu Entry 1',
-            id    : 'item_1'
-        },
-        {
-            text  : 'Menu Entry 2',   
-            id    : 'item_2'
-        },
-        {
-            text  : 'Menu Entry 3',
-            id    : 'item_3'
-        },
-        {
-            text  : 'Menu Entry 4',
-            id    : 'item_4'
-        }
-    ];
-    //the softkeys are defined in an array
-    var softkeys = [
-        {
-            label     : 'Select',
-            actionId  : 'select',
-            icon      : app.images.softKeys.select
-        },
-        {
-            label     : 'Help',
-            actionId  : 'help'
-        },
-        {
-            label     : 'Info',
-            actionId  : 'getInfo'
-        },
-        {
-            label     : 'Cancel',
-            actionId  : 'exit'
-        }
-    ];
-    //show the menu
-    genericMenu.show({
-        id            : 'menu_1',
-        menu          : items,
-        object        : handler,
-        title         : 'Menu Title',
-        softkeys      : softkeys,
-        onkeyselect   : selectCallback,
-        onkeycancel   : digium.background
-    });
-};
+digium.event.observe({
+    'eventName' : 'digium.app.foreground',
+    'callback'  : function() {
+        var handler = {};
+        //This handler is called when any assigned softkeys/hardkeys are pressed
+        //It receives the id of the selected item and the action as parameters
+        handler.processMenuAction = function (params) {
+            switch (params.actionId) {
+            case 'select':
+                selectCallback(params.selectionId);
+                break;
+            case 'help':
+                helpCallback(params.selectionId);
+                break;
+            case 'getInfo':
+                getInfo(params.selectionId);
+                break;
+            case 'exit':
+            default:
+                digium.background();
+                break;
+            }
+        };
+        //the menu items are defined in an array
+        var items = [
+            {
+                text  : 'Menu Entry 1',
+                id    : 'item_1'
+            },
+            {
+                text  : 'Menu Entry 2',   
+                id    : 'item_2'
+            },
+            {
+                text  : 'Menu Entry 3',
+                id    : 'item_3'
+            },
+            {
+                text  : 'Menu Entry 4',
+                id    : 'item_4'
+            }
+        ];
+        //the softkeys are defined in an array
+        var softkeys = [
+            {
+                label     : 'Select',
+                actionId  : 'select',
+                icon      : app.images.softKeys.select
+            },
+            {
+                label     : 'Help',
+                actionId  : 'help'
+            },
+            {
+                label     : 'Info',
+                actionId  : 'getInfo'
+            },
+            {
+                label     : 'Cancel',
+                actionId  : 'exit'
+            }
+        ];
+        //show the menu
+        genericMenu.show({
+            id            : 'menu_1',
+            menu          : items,
+            object        : handler,
+            title         : 'Menu Title',
+            softkeys      : softkeys,
+            onkeyselect   : selectCallback,
+            onkeycancel   : digium.background
+        });
+    }
+});
