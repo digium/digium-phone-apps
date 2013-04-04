08444f5e061a2d0e0bd8df50d1432c76e0210df1
Shaun Ruffell <sruffell@digium.com>
2013-04-04 03:39:35 -0500

    helloworld: Add Makefile / fix lint warning.

--- helloworld/helloworld.js
+++ helloworld/helloworld.js
@@ -2,4 +2,4 @@ var app = require('app');
 app.init();
 var screen = require('screen'); screen.clear();
 var hello = new Text(0, 0, window.w, Text.LINE_HEIGHT, 'Hello World');
-window.add(hello)
+window.add(hello);
